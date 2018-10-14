ea6be1596efcde20b7b6999ede1c4c03f1f49ff3
inad9300 <inad9300@gmail.com>
2018-10-14 02:50:43 +0200

    Preparing next release - bug fixes and little usability improvements

--- chrome/main.js
+++ chrome/main.js
@@ -1,77 +1,128 @@
 /**
  * Main program.
  */
-(function () {
-    setup_elem_picker()
-    listen_and_propagate_input_changes()
-})()
+setup_elem_picker()
+listen_and_propagate_input_changes()
 
 /**
- * Let the user pick an element and replace all the numbers on it with input boxes.
+ * Let the user pick a DOM element and replace all the numbers on it with input boxes.
  */
 function setup_elem_picker() {
     let last_hovered_elem
-    let elem_was_selected = false
 
-    on(document.body, 'mouseover', event => {
+    const off_mouseover = on(document.body, 'mouseover', on_mouseover)
+    const off_mouseout = on(document.body, 'mouseout', on_mouseout)
+    const off_click = on(document.body, 'click', on_click)
+
+    function on_mouseover(event) {
         last_hovered_elem = event.target
+        add_bg(event.target)
+    }
 
-        if (!elem_was_selected) event.target.classList.add('ingredient-list-selector')
-    })
+    function on_mouseout(event) {
+        remove_bg(event.target)
+    }
 
-    on(document.body, 'mouseout', event => remove_bg(event.target))
+    function on_click(event) {
+        remove_bg(last_hovered_elem)
+        replace_quantities_with_inputs(last_hovered_elem)
 
-    on(document.body, 'click', event => {
-        if (!elem_was_selected) {
-            remove_bg(last_hovered_elem)
-            replace_quantities_with_inputs(last_hovered_elem)
-        }
-        elem_was_selected = true
-    })
+        off_mouseover()
+        off_mouseout()
+        off_click()
+    }
+
+    function add_bg(elem) {
+        elem.classList.add('ra-ingredient-list-selector')
+    }
 
     function remove_bg(elem) {
-        elem.classList.remove('ingredient-list-selector')
+        elem.classList.remove('ra-ingredient-list-selector')
     }
-}
 
-/**
- * Shortcut for listening events on HTML elements.
- */
-function on(elem, event, fn) {
-    elem.addEventListener(event, fn)
-}
+    /**
+     * Replace all digits found within the given element with input boxes.
+     */
+    function replace_quantities_with_inputs(elem) {
+        const qty_regexp = /([1-9]\d*)?\s*(½|⅓|⅔|¼|¾|⅕|⅖|⅗|⅘|⅙|⅚|⅐|⅛|⅜|⅝|⅞|⅑|⅒)|([1-9]\d*)?\s*[1-9]\d*\s*\/\s*[1-9]\d*|[1-9]\d*(\.\d+)?|\b[Hh]alf\b/g
+        const walker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT)
 
-/**
- * Replace digits found within the given element with input boxes.
- */
-function replace_quantities_with_inputs(elem) {
-    const regex = /[1-9]\/[2-9]|\d+|¼|⅓|½|¾|half/g
-    const walker = document.createTreeWalker(elem, NodeFilter.SHOW_TEXT)
+        while (walker.nextNode()) {
+            let match
+            while ((match = qty_regexp.exec(walker.currentNode.nodeValue))) {
+                const original_val = match[0]
+                console.log('original_val', original_val)
+                const val_str = get_parseable_val(original_val)
+                const val_num = eval(val_str) // Only what fits the restricted regular expression is evaluated.
+                if (isNaN(val_num) || !isFinite(val_num)) {
+                    return
+                }
 
-    while (walker.nextNode()) {
-        let match
-        while ((match = regex.exec(walker.currentNode.nodeValue))) {
-            const value = get_numeric_value(match[0])
+                const span = document.createElement('span')
+                span.classList.add('ra-ingredient-input')
+                span.setAttribute('contenteditable', 'true')
+                span.setAttribute('title', 'Original value: ' + original_val)
+                span.setAttribute('data-initial-val', val_num)
+                span.setAttribute('data-initial-val-was-fraction', val_str.indexOf('/') > -1)
 
-            const span = document.createElement('span')
-            span.textContent = value
-            span.setAttribute('class', 'ingredient-input')
-            span.setAttribute('data-initial-value', value)
-            span.setAttribute('contenteditable', 'true')
+                // Remove the original string and insert the new input box.
+                const second_node = walker.currentNode.splitText(match.index)
+                second_node.nodeValue = second_node.nodeValue.substr(match[0].length)
+                walker.currentNode.parentNode.insertBefore(span, second_node)
 
-            const second_node = walker.currentNode.splitText(match.index)
-            second_node.nodeValue = second_node.nodeValue.substr(match[0].length) // Remove the original matched string.
-            walker.currentNode.parentNode.insertBefore(span, second_node)
-            walker.nextNode() // Skip the newly created node.
+                // Calculate the textual content only after the input is placed in the DOM.
+                span.textContent = get_new_val_str(span, 1)
+
+                // Skip the newly inserted input box.
+                walker.nextNode()
+            }
         }
-    }
 
-    function get_numeric_value(value) {
-        if (value === '¼') return '1/4'
-        else if (value === '⅓') return '1/3'
-        else if (value === '½' || value === 'half') return '1/2'
-        else if (value === '¾') return '3/4'
-        else return value
+        const ingredient_inputs = elem.getElementsByClassName('ra-ingredient-input')
+        if (ingredient_inputs.length > 0) {
+            ingredient_inputs[0].focus()
+            select(ingredient_inputs[0])
+        }
+
+        /**
+         * Replace non-numeric characters in the given string with their numeric counterparts.
+         */
+        function get_parseable_val(val_str) {
+            // Add a plus sign to fractions in case there is a number in front of them, for later evaluation.
+            return val_str
+                .replace('½', '+1/2')
+                .replace('⅓', '+1/3')
+                .replace('⅔', '+2/3')
+                .replace('¼', '+1/4')
+                .replace('¾', '+3/4')
+                .replace('⅕', '+1/5')
+                .replace('⅖', '+2/5')
+                .replace('⅗', '+3/5')
+                .replace('⅘', '+4/5')
+                .replace('⅙', '+1/6')
+                .replace('⅚', '+5/6')
+                .replace('⅐', '+1/7')
+                .replace('⅛', '+1/8')
+                .replace('⅜', '+3/8')
+                .replace('⅝', '+5/8')
+                .replace('⅞', '+7/8')
+                .replace('⅑', '+1/9')
+                .replace('⅒', '+1/10')
+                .replace(/(\d+)\s+(\d+)\/(\d+)/g, '$1+$2/$3')
+                .replace(/\s+/g, '')
+                .replace(/[Hh]alf/, '1/2')
+        }
+
+        /**
+         * Focus and select the contents of a DOM element.
+         */
+        function select(elem) {
+            const range = document.createRange()
+            range.selectNodeContents(elem)
+            const selection = window.getSelection()
+            selection.removeAllRanges()
+            selection.addRange(range)
+        }
     }
 }
 
@@ -80,71 +131,124 @@ function replace_quantities_with_inputs(elem) {
  */
 function listen_and_propagate_input_changes() {
     on(document.body, 'keyup', event => {
-        if (!event.target.classList.contains('ingredient-input')) return
-
-        const initial_value = parse_number(event.target.getAttribute('data-initial-value'))
-        const current_value = parse_number(event.target.textContent)
-        const ratio = current_value / initial_value
-
-        const ingredient_inputs = document.getElementsByClassName('ingredient-input')
-
-        for (let i = 0; i < ingredient_inputs.length; i++)
-            if (ingredient_inputs[i] !== event.target)
-                ingredient_inputs[i].textContent = get_new_value(ingredient_inputs[i], ratio)
-    })
-
-    function parse_number(str) {
-        return /[1-9]\/[2-9]/.test(str)
-            ? str_fraction_to_float(str)
-            : parseFloat(str.replace(/[^0-9\.]/g, ''))
-    }
-
-    function str_fraction_to_float(str) {
-        const [first_digit, second_digit] = str.split('/')
-        return parseInt(first_digit) / parseInt(second_digit)
-    }
-
-    function get_new_value(input, ratio) {
-        const initial_value = input.getAttribute('data-initial-value')
-        const new_value = ratio * parse_number(initial_value)
-        const new_value_has_decimals = new_value % 1 !== 0
-
-        // If the original value was a fraction or the ingredient is expressed in certain units, we return a fraction.
-        if (new_value_has_decimals && (contains_measuring_spoon(input.parentNode.textContent) || /[1-9]\/[2-9]/.test(initial_value))) {
-            const fraction_or_number = get_fraction_or_number(new_value)
-            if (/[1-9]\/[2-9]/.test(fraction_or_number)) return fraction_or_number
+        if (!event.target.classList.contains('ra-ingredient-input')) {
+            return
         }
 
-        const new_value_fixed = new_value.toFixed(1)
-        return new_value_fixed.endsWith('.0') ? new_value_fixed.slice(0, -2) : new_value_fixed
+        const initial_val = get_initial_val(event.target)
+        const current_val = str_to_num(event.target.textContent)
+        const ratio = current_val / initial_val
+        if (isNaN(ratio) || !isFinite(ratio)) {
+            return
+        }
+
+        const ingredient_inputs = document.getElementsByClassName('ra-ingredient-input')
+
+        for (const input of ingredient_inputs) {
+            if (input !== event.target) {
+                input.textContent = get_new_val_str(input, ratio)
+            }
+        }
+    })
+
+    /**
+     * Convert from a string of the form "X/Y", "X.Y" or "XY" to a number.
+     *
+     * FIXME The user could copy/paste a fraction, or put a number, space and fraction. This should therefore get the same treatment as the original values.
+     */
+    function str_to_num(str) {
+        const clean_str = str.replace(/[^\d]$/, '')
+        return clean_str.indexOf('/') > -1
+            ? fraction_to_float(clean_str)
+            : parseFloat(clean_str)
+
+        /**
+         * Convert from a string of the form "X/Y" to the corresponding float number.
+         */
+        function fraction_to_float(str) {
+            const [integer, fractional] = str.split('/')
+            return parseInt(integer) / parseInt(fractional)
+        }
+    }
+}
+
+/**
+ * Get a new value for the given input, in a readable form, considering its initial value and the conversion ratio.
+ */
+function get_new_val_str(input, ratio) {
+    const initial_val = get_initial_val(input)
+    const initial_val_was_fraction = input.getAttribute('data-initial-val-was-fraction') === 'true'
+    const new_val = initial_val * ratio
+
+    if (initial_val_was_fraction || contains_measuring_spoon(input.parentNode.textContent)) {
+        const fr = get_nice_fraction(new_val)
+        if (fr) {
+            return fr
+        }
     }
 
-    function contains_measuring_spoon(text) {
-        const measuring_spoons = ['tablespoon', 'tablespoons', 'tbsp', 'cup', 'cups', 'teaspoon', 'teaspoon', 'tsp']
+    const new_val_str = new_val.toFixed(1)
+    return new_val_str.endsWith('.0')
+        ? new_val_str.slice(0, -2)
+        : new_val_str
 
-        for (let i = 0; i < measuring_spoons.length; ++i)
-            if (text.indexOf(' ' + measuring_spoons[i]) > -1) return true
-        
+    function contains_measuring_spoon(text) {
+        const measuring_spoon_regexps = ['tablespoon', 'tbsp', 'cup', 'teaspoon', 'tsp'].map(spoon => {
+            return new RegExp('\\b' + spoon + 's?\\b', 'gi')
+        })
+
+        for (const regexp of measuring_spoon_regexps) {
+            if (regexp.test(text)) {
+                return true
+            }
+        }
         return false
     }
 
-    function get_fraction_or_number(number) {
-        const sensible_fractions_str = [
-            '1/2',
-            '1/3', '2/3',
-            '1/4', '2/4', '3/4',
-            '1/5', '2/5', '3/5', '4/5',
-            '1/6', '2/6', '3/6', '4/6', '5/6',
-            '1/7', '2/7', '3/7', '4/7', '5/7', '6/7',
-            '1/8', '2/8', '3/8', '4/8', '5/8', '6/8', '7/8',
-            '1/9', '2/9', '3/9', '4/9', '5/9', '6/9', '7/9', '8/9',
-            '1/10','2/10','3/10','4/10','5/10','6/10','7/10','8/10','9/10'
-        ]
-        const sensible_fractions_num = sensible_fractions_str.map(str_fraction_to_float)
+    /**
+     * Get a shorter representation of a fraction, if available and is reasonable.
+     */
+    function get_nice_fraction(num) {
+        const sensible_fractions = {
+            '½': 1 / 2,
+            '⅓': 1 / 3,
+            '⅔': 2 / 3,
+            '¼': 1 / 4,
+            '¾': 3 / 4,
+            '⅕': 1 / 5,
+            '⅖': 2 / 5,
+            '⅒': 1 / 10
+        }
 
-        for (let i = 0; i < sensible_fractions_num.length; ++i)
-            if (number === sensible_fractions_num[i]) return sensible_fractions_str[i]
-        
-        return number
+        for (const fraction in sensible_fractions) {
+            if (ish(num, sensible_fractions[fraction])) {
+                return fraction
+            }
+        }
+
+        /**
+         * Check if two numbers are approximately the same, avoiding precission problems.
+         */
+        function ish(n, m) {
+            return +n.toFixed(16) === +m.toFixed(16)
+        }
+    }
+}
+
+/**
+ * Read back the numeric version of the original value.
+ */
+function get_initial_val(elem) {
+    return parseFloat(elem.getAttribute('data-initial-val'))
+}
+
+/**
+ * Shortcut for listening events on HTML elements.
+ */
+function on(elem, event, fn) {
+    elem.addEventListener(event, fn)
+
+    return function off() {
+        elem.removeEventListener(event, fn)
     }
 }
